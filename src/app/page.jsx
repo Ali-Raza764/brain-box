27b642b68e7649050775c51ca67838306f71f17a
Ali-Raza764 <alikillerno@gmail.com>
2024-10-13 19:54:15 +0500

    Add revalidation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,18 @@ import PDFList from "@/components/ui/PDFList";
 import { connectDb } from "@/lib/db";
 import PDFItem from "@/lib/models/pdfItem.model";
 
+export const revalidate = 100;
+
 const Home = async () => {
-  await connectDb();
-  const pdfItems = await PDFItem.find({}).exec();
+  let pdfItems = [];
+  try {
+    await connectDb();
+    pdfItems = await PDFItem.find({}).exec();
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (!pdfItems) return <div>No PDF found Some error occured</div>;
 
   return (
     <div className="min-h-screen bg-gray-100 flex justify-center">
